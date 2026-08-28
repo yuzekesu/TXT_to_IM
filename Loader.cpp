e1c8edde186765ad6b06875678836fad1c6a5cba
xX_DarkLorD_Xx <yuzekesu@gmail.com>
2026-08-28 22:57:09 +0200

    fix bug where the last line of the .txt may not be read because of lacking the linebreak.

--- Loader.cpp
+++ Loader.cpp
@@ -18,12 +18,9 @@ std::tuple <std::vector<Coordinate>, std::vector<uint32_t>> Loader::Load() {
 	// 1. get the line.
 	// 2. split to words.
 	// 3. take the first three words and convert them to float.
-	for (unsigned long long i = 0; i < std::numeric_limits<unsigned long long>::max(); ++i) {
+	while (f.getline(buffer, sizeof(buffer) / sizeof(buffer[0]))) {
 		// 1
-		f.getline(buffer, sizeof(buffer) / sizeof(buffer[0]));
 		++lineCounter;
-		if (f.eof()) throw std::runtime_error(std::format("No indices detected at the line {} in the .txt", lineCounter));
-		if (f.fail()) throw std::runtime_error(std::format("Containing to many characters at the line {}. Maximum characters allowed is {}.", lineCounter, sizeof(buffer) / sizeof(buffer[0])));
 		// 2 & 3
 		std::wistringstream stream{ buffer };
 		std::wstring word;
@@ -49,29 +46,28 @@ std::tuple <std::vector<Coordinate>, std::vector<uint32_t>> Loader::Load() {
 		// the size of the vector here is exactly three.
 		vertices.push_back({ .x = tempFloatVector[0], .y = tempFloatVector[1], .z = tempFloatVector[2] });
 	}
-
+	if (f.eof()) throw std::runtime_error(std::format("No indices detected at the line {} in the .txt", lineCounter));
+	if (f.fail()) throw std::runtime_error(std::format("Containing to many characters at the line {}. Maximum characters allowed is {}.", lineCounter, sizeof(buffer) / sizeof(buffer[0])));
 	// parse the indices.
 	// 1. get the line.
 	// 2. split to words.
 	// 3. convert them to uint32_t.
 	std::vector<uint32_t> indices;
-	for (unsigned long long i = 0; i < std::numeric_limits<unsigned long long>::max(); ++i) {
+	while (f.getline(buffer, sizeof(buffer) / sizeof(buffer[0]))) {
 		// 1
-		f.getline(buffer, sizeof(buffer) / sizeof(buffer[0]));
 		++lineCounter;
-		if (f.eof()) break;
-		if (f.fail()) throw std::runtime_error(std::format("Containing to many characters at the line {}. Maximum characters allowed is {}.", lineCounter, sizeof(buffer) / sizeof(buffer[0])));
 		// 2 & 3
 		std::wistringstream stream{ buffer };
 		std::wstring word;
 		while (stream >> word) {
 			try {
-				indices.push_back(static_cast<uint32_t>(std::stol(word)));
+				indices.push_back(static_cast<uint32_t>(std::stoul(word)));
 			}
 			catch (...) {
 				// let it be ;)
 			}
 		}
 	}
+
 	return { std::move(vertices), std::move(indices) };
 }
